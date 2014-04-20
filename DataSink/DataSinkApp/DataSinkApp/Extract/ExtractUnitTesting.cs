2c5667130c22a3fef9a0c86f4b3eefae842a17dc
viqas-hussain-15below <viqas.hussain@15below.com>
2014-04-20 11:00:41 +0100

    DataSinkApp - Edit Config Settings to correct Values

--- DataSink/DataSinkApp/DataSinkApp/Extract/ExtractUnitTesting.cs
+++ DataSink/DataSinkApp/DataSinkApp/Extract/ExtractUnitTesting.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace DataSinkApp.Extract
 {
+    [TestFixture]
     class ExtractUnitTesting
     {
+
+        [TestCase]
+        public void AddTest()
+        {
+
+        }
     }
 }
